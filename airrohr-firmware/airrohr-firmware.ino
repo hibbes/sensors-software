d5ab986ffd5b168270d5f48ae26d312373a4d552
Marek Czernohous <mczernohous@gmail.com>
2026-05-12 03:13:39 +0200

    refactor: Phase B (6/8) — webserver_status nach web/pages/status.cpp (Issue #18)
    Status-Page in eigenes Module + page_helpers.h massiv erweitert um die
    extern-Decls/Forward-Decls, die status/values/config alle teilen:
    
    - 60+ last_value_* Sensor-Werte (float/double/uint16_t/String)
    - Counters: SDS_/NPM_/IPS_/SPS30_/WiFi_error_count, count_sends, sending_time,
      starttime, time_point_device_start_ms, last_update_attempt, last_update_returncode,
      last_sendData_returncode, last_signal_strength, last_disconnect_reason
    - airrohr_selftest_failed, sntp_time_set, SOFTWARE_VERSION (String)
    - cfg-Namespace ~75 Felder (read+write seitens config) — Block VOR airrohr-cfg.h
      inkludiert, weil configShape[] in airrohr-cfg.h cfg::*-Adressen referenziert
    - BMX280 bmx280, SCD30 scd30 Instanzen
    - LoggerConfig loggerConfigs[LoggerCount]
    - writeConfig, dew_point, pressure_at_sealevel, add_form_input, form_checkbox,
      form_submit, form_select_lang, add_warning_first_cycle, add_age_last_values
      von static → external linkage (Definitions bleiben in .ino bis Phase C)
    
    Build nodemcuv2_de: Flash 69.3% (-2860 B vs. vorigem Stand!), RAM 43.7%.
    airrohr-firmware.ino: 5043 Z. (von 5188, -145).
    
    Refs: hibbes/sensors-software#18

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -1302,7 +1302,7 @@ static void init_config()
 /*****************************************************************
  * write config to spiffs                                        *
  *****************************************************************/
-static bool writeConfig()
+bool writeConfig()
 {
 	DynamicJsonDocument json(JSON_BUFFER_SIZE);
 	debug_outln_info(F("Saving config..."));
@@ -1404,7 +1404,7 @@ static void createLoggerConfigs()
 /*****************************************************************
  * dew point helper function                                     *
  *****************************************************************/
-static float dew_point(const float temperature, const float humidity)
+float dew_point(const float temperature, const float humidity)
 {
 	float dew_temp;
 	const float k2 = 17.62;
@@ -1418,7 +1418,7 @@ static float dew_point(const float temperature, const float humidity)
 /*****************************************************************
  * dew point helper function                                     *
  *****************************************************************/
-static float pressure_at_sealevel(const float temperature, const float pressure)
+float pressure_at_sealevel(const float temperature, const float pressure)
 {
 	float pressure_at_sealevel;
 
@@ -1467,7 +1467,7 @@ void end_html_page(String &page_content)
 	server.sendContent_P(WEB_PAGE_FOOTER);
 }
 
-static void add_form_input(String &page_content, const ConfigShapeId cfgid, const __FlashStringHelper *info, const int length)
+void add_form_input(String &page_content, const ConfigShapeId cfgid, const __FlashStringHelper *info, const int length)
 {
 	RESERVE_STRING(s, MED_STR);
 	s = F("<tr>"
@@ -1508,7 +1508,7 @@ static void add_form_input(String &page_content, const ConfigShapeId cfgid, cons
 	page_content += s;
 }
 
-static String form_checkbox(const ConfigShapeId cfgid, const String &info, const bool linebreak)
+String form_checkbox(const ConfigShapeId cfgid, const String &info, const bool linebreak)
 {
 	RESERVE_STRING(s, MED_STR);
 	s = F("<label for='{n}'>"
@@ -1532,7 +1532,7 @@ static String form_checkbox(const ConfigShapeId cfgid, const String &info, const
 	return s;
 }
 
-static String form_submit(const String &value)
+String form_submit(const String &value)
 {
 	String s = F("<tr>"
 				 "<td>&nbsp;</td>"
@@ -1544,7 +1544,7 @@ static String form_submit(const String &value)
 	return s;
 }
 
-static String form_select_lang()
+String form_select_lang()
 {
 	String s_select = F(" selected='selected'");
 	String s = F("<tr>"
@@ -1589,7 +1589,7 @@ static String form_select_lang()
 	return s;
 }
 
-static void add_warning_first_cycle(String &page_content)
+void add_warning_first_cycle(String &page_content)
 {
 	String s = FPSTR(INTL_TIME_TO_FIRST_MEASUREMENT);
 	unsigned int time_to_first = cfg::sending_intervall_ms - msSince(starttime);
@@ -1601,7 +1601,7 @@ static void add_warning_first_cycle(String &page_content)
 	page_content += s;
 }
 
-static void add_age_last_values(String &s)
+void add_age_last_values(String &s)
 {
 	s += "<b>";
 	unsigned int time_since_last = msSince(starttime);
@@ -2385,151 +2385,6 @@ static void webserver_values()
 	end_html_page(page_content);
 }
 
-/*****************************************************************
- * Webserver root: show device status
- *****************************************************************/
-static void webserver_status()
-{
-	if (WiFi.status() != WL_CONNECTED)
-	{
-		sendHttpRedirect();
-		return;
-	}
-
-	RESERVE_STRING(page_content, XLARGE_STR);
-	start_html_page(page_content, FPSTR(INTL_DEVICE_STATUS));
-
-	debug_outln_info(F("ws: status ..."));
-	server.sendContent(page_content);
-	page_content = F("<table cellspacing='0' cellpadding='5' class='v'>\n"
-					 "<thead><tr><th> " INTL_PARAMETER "</th><th>" INTL_VALUE "</th></tr></thead>");
-	String versionHtml(SOFTWARE_VERSION);
-	versionHtml += F("/ST:");
-	versionHtml += String(!airrohr_selftest_failed);
-	versionHtml += '/';
-#if defined(ESP8266)
-	versionHtml += ESP.getFullVersion();
-#endif
-	versionHtml.replace("/", FPSTR(BR_TAG));
-	add_table_row_from_value(page_content, FPSTR(INTL_FIRMWARE), versionHtml);
-	add_table_row_from_value(page_content, F("Free Memory"), String(ESP.getFreeHeap()));
-#if defined(ESP8266)
-	add_table_row_from_value(page_content, F("Heap Fragmentation"), String(ESP.getHeapFragmentation()), "%");
-#endif
-	if (cfg::auto_update)
-	{
-		add_table_row_from_value(page_content, F("Last OTA"), delayToString(millis() - last_update_attempt));
-	}
-#if defined(ESP8266)
-	add_table_row_from_value(page_content, F("NTP Sync"), String(sntp_time_set));
-	StreamString ntpinfo;
-
-	for (unsigned i = 0; i < SNTP_MAX_SERVERS; i++)
-	{
-		const ip_addr_t *sntp = sntp_getserver(i);
-		if (sntp && !ip_addr_isany(sntp))
-		{
-			const char *name = sntp_getservername(i);
-			if (!ntpinfo.isEmpty())
-			{
-				ntpinfo.print(FPSTR(BR_TAG));
-			}
-			ntpinfo.printf_P(PSTR("%s (%s)"), IPAddress(*sntp).toString().c_str(), name ? name : "?");
-			ntpinfo.printf_P(PSTR(" reachable: %s"), sntp_getreachability(i) ? "Yes" : "No");
-		}
-	}
-	add_table_row_from_value(page_content, F("NTP Info"), ntpinfo);
-#endif
-
-	time_t now = time(nullptr);
-	add_table_row_from_value(page_content, FPSTR(INTL_TIME_UTC), ctime(&now));
-	add_table_row_from_value(page_content, F("Uptime"), delayToString(millis() - time_point_device_start_ms));
-#if defined(ESP8266)
-	add_table_row_from_value(page_content, F("Reset Reason"), ESP.getResetReason());
-#endif
-	if (cfg::sds_read)
-	{
-		page_content += FPSTR(EMPTY_ROW);
-		add_table_row_from_value(page_content, FPSTR(SENSORS_SDS011), last_value_SDS_version);
-	}
-	if (cfg::npm_read)
-	{
-		page_content += FPSTR(EMPTY_ROW);
-		add_table_row_from_value(page_content, FPSTR(SENSORS_NPM), last_value_NPM_version);
-	}
-	if (cfg::scd30_read)
-	{
-		if (scd30.getAutoSelfCalibration() == true)
-			add_table_row_from_value(page_content, F("SCD30 Auto Calibration"), "enabled");
-		else
-			add_table_row_from_value(page_content, F("SCD30 Auto Calibration"), "disabled");
-		uint16_t settingVal;
-		scd30.getMeasurementInterval(&settingVal);
-		add_table_row_from_value(page_content, F("SCD30 measurement interval"), String(settingVal));
-		scd30.getTemperatureOffset(&settingVal);
-		add_table_row_from_value(page_content, F("SCD30 temperature offset"), String(settingVal));
-	}
-
-	page_content += FPSTR(EMPTY_ROW);
-	page_content += F("<tr><td colspan='2'><b>" INTL_ERROR "</b></td></tr>");
-	String wifiStatus(WiFi_error_count);
-	wifiStatus += '/';
-	wifiStatus += String(last_signal_strength);
-	wifiStatus += '/';
-	wifiStatus += String(last_disconnect_reason);
-	add_table_row_from_value(page_content, F("WiFi"), wifiStatus);
-
-	if (last_update_returncode != 0)
-	{
-		add_table_row_from_value(page_content, F("OTA Return"),
-								 last_update_returncode > 0 ? String(last_update_returncode) : HTTPClient::errorToString(last_update_returncode));
-	}
-	for (unsigned int i = 0; i < LoggerCount; ++i)
-	{
-		if (loggerConfigs[i].errors)
-		{
-			const __FlashStringHelper *logger = loggerDescription(i);
-			if (logger)
-			{
-				add_table_row_from_value(page_content, logger, String(loggerConfigs[i].errors));
-			}
-		}
-	}
-
-	if (last_sendData_returncode != 0)
-	{
-		add_table_row_from_value(page_content, F("Data Send Return"),
-								 last_sendData_returncode > 0 ? String(last_sendData_returncode) : HTTPClient::errorToString(last_sendData_returncode));
-	}
-	if (cfg::sds_read)
-	{
-		add_table_row_from_value(page_content, FPSTR(SENSORS_SDS011), String(SDS_error_count));
-	}
-	if (cfg::npm_read)
-	{
-		add_table_row_from_value(page_content, FPSTR(SENSORS_NPM), String(NPM_error_count));
-	}
-	if (cfg::sps30_read)
-	{
-		add_table_row_from_value(page_content, FPSTR(SENSORS_SPS30), String(SPS30_read_error_counter));
-	}
-
-	server.sendContent(page_content);
-	page_content = emptyString;
-
-	if (count_sends > 0)
-	{
-		page_content += FPSTR(EMPTY_ROW);
-		add_table_row_from_value(page_content, F(INTL_NUMBER_OF_MEASUREMENTS), String(count_sends));
-		if (sending_time > 0)
-		{
-			add_table_row_from_value(page_content, F(INTL_TIME_SENDING_MS), String(sending_time), "ms");
-		}
-	}
-
-	page_content += FPSTR(TABLE_TAG_CLOSE_BR);
-	end_html_page(page_content);
-}
 
 /*****************************************************************
  * Webserver read serial ring buffer                             *
